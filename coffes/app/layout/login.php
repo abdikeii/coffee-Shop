7dad9a16154bb8b3a06e492b99302619814ad5ce
abdikeii <99780359+abdikeii@users.noreply.github.com>
2022-08-02 23:27:46 +0700

    first

---
+++ coffes/app/layout/login.php
@@ -0,0 +1,19 @@
+<div class="wrapper">
+    <div class="logo">
+        <img src="images/logo.png" alt="">
+    </div>
+    <div class="text-center mt-4 name">
+        Coffe's
+    </div>
+    <form class="p-3 mt-3" action="" method="POST">
+        <div class="form-field d-flex align-items-center">
+            <span class="far fa-user"></span>
+            <input type="text" name="email" id="userName" placeholder="Username">
+        </div>
+        <div class="form-field d-flex align-items-center">
+            <span class="fas fa-key"></span>
+            <input type="password" name="password" id="pwd" placeholder="Password" name="password">
+        </div>
+        <button class="btn mt-3" name="submit">Login</button>
+    </form>
+</div>
